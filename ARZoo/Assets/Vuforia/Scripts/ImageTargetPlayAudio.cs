c911223455202789b0ad253e17f5396e250c11ac
Melissa Obrou <melissaobrou@n147-p95.eduroam.kth.se>
2019-02-13 12:32:38 +0100

    image targets 2 and background sound

---
+++ ARZoo/Assets/Vuforia/Scripts/ImageTargetPlayAudio.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ImageTargetPlayAudio : MonoBehaviour,
+                                            ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+ 
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+     
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            // Play audio when target is found
+            audio.Play();
+        }
+        else
+        {
+            // Stop audio when target is lost
+            audio.Stop();
+        }
+    }  
+        // Update is called once per frame
+    //void Update()
+    //{
+        
+   //} 
+}
