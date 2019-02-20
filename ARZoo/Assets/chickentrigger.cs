06ae11f57aa7e14468769e38d952b141a795a660
Melissa Obrou <melissaobrou@n182-p185.eduroam.kth.se>
2019-02-20 11:44:06 +0100

    sound beginning to work

---
+++ ARZoo/Assets/chickentrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chickentrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<AudioSource>().Play();
+    }
+
+    // Update is called once per frame
+   // void Update()
+    //{
+        
+    //}
+}
