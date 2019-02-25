2a1bee09f081239c3d6d819c066ce2756a6aa309
Melissa Obrou <melissaobrou@n165-p14.eduroam.kth.se>
2019-02-25 11:58:14 +0100

    all targets present and sound recorded

--- ARZoo/Assets/chickentrigger.cs
+++ ARZoo/Assets/chickentrigger.cs
@@ -7,12 +7,16 @@ public class chickentrigger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.GetComponent<AudioSource>().Play();
+        //gameObject.GetComponent<AudioSource>().Play();
     }
 
     // Update is called once per frame
-   // void Update()
-    //{
+    void Update()
+    {
+         //if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began){
+
         
-    //}
+        if(Input.GetMouseButtonDown(0))
+         {gameObject.GetComponent<AudioSource>().Play();}
+    }
 }
---
+++ ARZoo/Assets/cowinfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cowinfo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<AudioSource>().Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began){
+
+        
+        //if(Input.GetMouseButtonDown(0))
+        //{gameObject.GetComponent<AudioSource>().Play();}
+
+    }
+}
